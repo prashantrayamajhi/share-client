ef94bec32e11736040d7df954c1e081fea4cbabc
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-10-09 15:03:33 +0545

    setup tailwind and scss

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const path = require("path");
+const withSass = require("@zeit/next-sass");
+
 module.exports = {
   reactStrictMode: true,
-}
+};
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
