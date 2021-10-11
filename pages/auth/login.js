bf65d05c59c1860fb80a419d2fbacd9f33e8c3f1
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-10-11 15:07:53 +0545

    redirect from login to verification page

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,21 +1,71 @@
 import Layout from "./../../components/Layout";
 import styles from "./../../styles/auth.module.scss";
 import Link from "next/link";
+import { useState } from "react";
+import Axios from "./../../api/server";
+import { ToastContainer, toast } from "react-toastify";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [err, setErr] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  if (err) {
+    toast.error(err, {
+      theme: "colored",
+    });
+  }
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const data = {
+      email,
+      password,
+    };
+    try {
+      const res = await Axios.post("/auth/login", data);
+      console.log(res);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      if (err.response.data.err === "Please verify your account through mail") {
+        window.location.href = "/verify/" + email;
+      } else {
+        setErr(err.response.data.err);
+        setErr(null);
+      }
+      setLoading(false);
+    }
+  };
   return (
     <Layout title="Login">
+      <ToastContainer />
       <div className={styles.wrapper}>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <h3>Login</h3>
           <div className={styles.input}>
-            <input type="email" placeholder="Enter your email address" />
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className={styles.input}>
-            <input type="password" placeholder="Enter your password" />
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className={styles.btn}>
-            <button>Login</button>
+            <button type="submit" disabled={loading}>
+              Login
+            </button>
           </div>
           <div className={styles.footer}>
             <p>
