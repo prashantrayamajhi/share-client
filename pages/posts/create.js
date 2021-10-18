925d8229cfd4bc9afe21b11307be58b3f8ab9686
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-10-18 15:14:15 +0545

    post an idea

--- pages/posts/create.js
+++ pages/posts/create.js
@@ -4,25 +4,72 @@ import { useEffect, useState } from "react";
 import { checkJwtToken } from "../../helper/jwt";
 import styles from "./../../styles/create.module.scss";
 import classnames from "classnames";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Axios from "./../../api/server";
+import { useRouter } from "next/router";
 
 const Post = () => {
+  const router = useRouter();
+
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [img, setImg] = useState("");
   const [isPrivate, setIsPrivate] = useState(false);
+  const [config, setConfig] = useState(null);
+  const [error, setError] = useState(null);
 
+  if (error) {
+    toast.error(error, {
+      theme: "colored",
+    });
+  }
   useEffect(() => {
     if (!checkJwtToken()) {
-      window.location.href = "/auth/login";
+      router.push("/auth/login");
+    } else {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+      };
+      setConfig(config);
     }
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = {
+        title,
+        content,
+        img,
+        isPrivate,
+      };
+      const res = await Axios.post("/user/posts", data, config);
+      if (res.status === 201) {
+        toast.success("Idea posted successfully", {
+          theme: "colored",
+        });
+        setTitle("");
+        setContent("");
+        setImg("");
+        setIsPrivate("");
+      }
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data.err);
+      setError(null);
+    }
+  };
+
   return (
     <>
+      <ToastContainer />
       <Navbar />
       <Layout title="Create Post">
         <div className={styles.wrapper}>
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={handleSubmit}>
             <h3>Share an Idea</h3>
             <div className={styles.input}>
               <label htmlFor="title">Title</label>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,14 +1,37 @@
 import Layout from "../../components/Layout";
 import Navbar from "../../components/Navbar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { checkJwtToken } from "../../helper/jwt";
+import { useRouter } from "next/router";
+import Axios from "./../../api/server";
 
 const Post = () => {
+  const [config, setConfig] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const router = useRouter();
+
   useEffect(() => {
     if (!checkJwtToken()) {
-      window.location.href = "/auth/login";
+      router.push("/auth/login");
+    } else {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+      };
+      setConfig(config);
     }
   }, []);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await Axios.get("/user/posts", config);
+      setPosts(res.data.data);
+    };
+    if (config) {
+      fetchPosts();
+    }
+  }, [config]);
 
   return (
     <>
