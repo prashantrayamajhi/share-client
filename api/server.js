6d9f54e9d89306561217a085219467245d0c6512
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2022-01-18 17:11:18 +0545

    connect to deployed api on production

--- api/server.js
+++ api/server.js
@@ -1,5 +1,7 @@
 const Axios = require("axios");
-const BASE_URL = "http://localhost:8080/api/v1";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+  ? process.env.NEXT_PUBLIC_API_URL
+  : "http://localhost:8080/api/v1";
 
 module.exports = Axios.create({
   baseURL: BASE_URL,
