747161cfa3f4eaf738b7523e89d2bde167ee83ce
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2022-02-19 20:39:27 +0545

    update border bottom for the containers

--- components/Utils/Container/Container.jsx
+++ components/Utils/Container/Container.jsx
@@ -1,29 +1,15 @@
-import { useRef, useEffect, useState } from "react";
-import classNames from "classnames";
 import styles from "./../../../styles/container.module.scss";
 
 const Container = ({ title, children }) => {
-  const [titleWidth, setTitleWidth] = useState("100%");
-  const titleRef = useRef(null);
-
-  useEffect(() => {
-    setTitleWidth(`${titleRef.current.offsetWidth}px`);
-  }, [titleRef]);
-
   return (
     <>
       <div className={styles.container}>
         <div className={styles.title}>
-          <h2 ref={titleRef}>{title}</h2>
-          <div
-            className={styles.border}
-            style={{
-              width: titleWidth,
-            }}
-          >
-            <div className={classNames(styles.dot, styles.left)}></div>
-            <div className={classNames(styles.dot, styles.right)}></div>
-          </div>
+          <h2>
+            {title}
+            <span className={styles.right}></span>
+            <span className={styles.left}></span>
+          </h2>
         </div>
         <div className={styles.content}>{children}</div>
       </div>
