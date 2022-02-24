481251d7064beceb9bd8a4eb91416c1e4552a7fa
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2022-02-24 16:19:52 +0545

    Update cards styles on home page

--- components/Home/Cards/Card.jsx
+++ components/Home/Cards/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ img, title, description, btn, link }) => {
       <Link href={link}>
         <div className={styles.card}>
           <img src={img} alt={title} />
-          <div className={styles.content}>
+          <div className={styles.content}> 
             <h3>{title}</h3>
             <p>{description}</p>
             {btn && <button>{btn}</button>}
