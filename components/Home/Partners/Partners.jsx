5d253b9ea480ec747527dd9a89e5e986dac09c46
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2022-03-22 12:37:14 +0545

    display partners from backend

--- components/Home/Partners/Partners.jsx
+++ components/Home/Partners/Partners.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
 import styles from "./../../../styles/home/partners.module.scss";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import Axios from "./../../../api/server";
 
 const Partners = () => {
+  const [partners, setPartners] = useState([]);
+
+  useEffect(() => {
+    const fetchPartners = async () => {
+      const res = await Axios.get("/partners");
+      setPartners(res.data.data);
+    };
+    fetchPartners();
+  }, []);
+
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -18,52 +30,32 @@ const Partners = () => {
       items: 1,
     },
   };
-  const partners = [
-    {
-      id: 1,
-      img: "/paral.png",
-    },
 
-    {
-      id: 5,
-      img: "/paral.png",
-    },
-    {
-      id: 4,
-      img: "/paral.png",
-    },
-    {
-      id: 3,
-      img: "/paral.png",
-    },
-    {
-      id: 2,
-      img: "/paral.png",
-    },
-  ];
   return (
     <>
       <div className={styles.wrapper}>
         <h2>Our Partners</h2>
-        <Carousel
-          responsive={responsive}
-          infinite={true}
-          keyBoardControl={true}
-          arrows={false}
-          showDots={true}
-          removeArrowOnDeviceType={["tablet", "mobile"]}
-          containerClass={styles.carousel}
-          autoPlay={true}
-          autoPlaySpeed={3000}
-        >
-          {partners.map((partner) => {
-            return (
-              <div className={styles.item} key={partner.id}>
-                <img src={partner.img} alt="partner" />
-              </div>
-            );
-          })}
-        </Carousel>
+        {partners.length > 0 && (
+          <Carousel
+            responsive={responsive}
+            infinite={true}
+            keyBoardControl={true}
+            arrows={false}
+            showDots={true}
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            containerClass={styles.carousel}
+            autoPlay={true}
+            autoPlaySpeed={3000}
+          >
+            {partners.map((partner) => {
+              return (
+                <div className={styles.item} key={partner.id}>
+                  <img src={partner.image} alt="partner" />
+                </div>
+              );
+            })}
+          </Carousel>
+        )}
       </div>
     </>
   );
